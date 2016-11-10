02095443386523e54c43b7c828081334eb510b38
Stephan <st.neupert@web.de>
2016-11-10 22:26:31 +0100

    Loading syles implemented: Part II

---
+++ NLEditor/LoadFromFile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace NLEditor
+{
+    static class LoadFromFile
+    {
+        public static string CreatePieceKey(string FilePath)
+        {
+            return Path.GetFullPath(FilePath).Remove(0, C.AppPathPieces.Length);
+        }
+
+        public static string CreatePieceKey(string StyleName, string PieceName, bool IsObject)
+        {
+            return StyleName + C.DirSep + (IsObject ? "objects" : "terrain") 
+                             + C.DirSep + PieceName + ".png";
+        }
+
+        public static List<Color> StyleColors(string StyleName)
+        {
+            string FilePath = C.AppPath + "styles" + C.DirSep + "themes" + C.DirSep;
+            
+            List<Color> ColorList = new List<Color>();
+            // Write default colors in it
+            ColorList.Add(Color.Black);
+
+            // return default list if no further infos exist
+            if (!File.Exists(FilePath)) return ColorList;
+
+            // otherwise read the file
+            try
+            {
+                using (StreamReader Stream = new StreamReader(FilePath))
+                {
+                    string Line;
+                    while ((Line = Stream.ReadLine()) != null)
+                    {
+                        if (Line.Trim().Substring(0, 10).ToUpper() == "BACKGROUND")
+                        {
+                            ColorList[0] = ColorTranslator.FromHtml("#" + Line.Trim().Substring(10).Trim());
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                // do nothing
+            }
+
+            return ColorList;
+        }
+
+        public static List<string> OrderStyleNames(List<string> StyleNames)
+        {
+            string FilePath = C.AppPath + "styles" + C.DirSep + "styles.ini";
+
+            if (!File.Exists(FilePath)) return StyleNames;
+            
+            // Otherwise order the styles according to styles.ini
+            List<string> NewStyleNames = new List<string>();
+
+            // TODO
+            NewStyleNames = StyleNames;
+
+            return NewStyleNames;
+        }
+
+
+    }
+}
---
+++ NLEditor/Style.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NLEditor
+{
+    public class Style
+    {
+        /*---------------------------------------------------------
+         *          This class stores all style infos
+         * -------------------------------------------------------- */
+
+        public Style(string StyleName)
+        {
+            this.fFileName = StyleName;
+            this.fName = StyleName; // may be overwritten later when forming the StyleList
+
+            SearchDirectoryForTerrain();
+            SearchDirectoryForObjects();
+            List<Color> StyleColors = LoadFromFile.StyleColors(StyleName);
+            fBackgroundColor = StyleColors[0];
+        }
+
+        string fFileName; // name that is used in the directory
+        string fName; // name that is displayed in the editor
+        List<string> fPieceNames;
+        List<string> fObjectNames;
+        Color fBackgroundColor;
+
+        public string FileName { get { return fFileName; } }
+        public string Name { get { return fName; } set { fName = value; } }
+        public List<string> PieceNames { get { return fPieceNames; } }
+        public List<string> ObjectNames { get { return fObjectNames; } }
+        public Color BackgroundColor { get { return fBackgroundColor; } }
+
+
+        private void SearchDirectoryForTerrain()
+        {
+            string DirectoryPath = C.AppPathPieces + FileName + C.DirSep + "terrain";
+
+            fPieceNames = Directory.GetFiles(DirectoryPath, "*.png", SearchOption.TopDirectoryOnly)
+                                   .Select(file => LoadFromFile.CreatePieceKey(file))
+                                   .ToList();
+        }
+
+        private void SearchDirectoryForObjects()
+        {
+            // Load first the default objects into the list
+            string DirectoryPath = C.AppPathPieces + "default" + C.DirSep + "objects";
+
+            try
+            {
+                fObjectNames = Directory.GetFiles(DirectoryPath, "*.png", SearchOption.TopDirectoryOnly)
+                                        .Select(file => LoadFromFile.CreatePieceKey(Path.GetFullPath(file)))
+                                        .ToList();
+            }
+            catch (Exception Ex)
+            {
+                String ErrorPath = C.AppPath + "ErrorLog.txt";
+                TextWriter TextFile = new StreamWriter(ErrorPath, true);
+                TextFile.WriteLine(Ex.ToString());
+                TextFile.Close();
+
+                MessageBox.Show(Ex.Message);
+
+                // Continue ignoring missing default style
+                fObjectNames = new List<string>();
+            }
+
+            // Load now the style-specific objects
+            DirectoryPath = C.AppPathPieces + FileName + C.DirSep + "objects";
+
+            fObjectNames.AddRange(Directory.GetFiles(DirectoryPath, "*.png", SearchOption.TopDirectoryOnly)
+                                           .Select(file => LoadFromFile.CreatePieceKey(Path.GetFullPath(file)))
+                                           .ToList());
+        }
+    }
+}
