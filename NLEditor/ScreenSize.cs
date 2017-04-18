2c4be18d08cffc2fe872954cb1c5fb4a29edf38d
Stephan <st.neupert@web.de>
2017-04-18 14:45:17 +0200

    First version of the class to compute the area of the level that will be displayed on the screen.

---
+++ NLEditor/ScreenSize.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace NLEditor
+{
+    /// <summary>
+    /// This class reads the NeoLemmix player's settings
+    /// and determines how much of the level gets displayed
+    /// </summary>
+    class ScreenSize
+    {
+        public ScreenSize()
+        {
+            SetDefault();
+        }
+
+        Size windowSize;
+        Size windowSizeWithPanel;
+        int zoomFactor;
+        bool isFullScreen;
+        bool isFillBorder;
+        bool isCompactSkillPanel;
+
+        private int skillPanelWidth => isCompactSkillPanel ? 320 : 416;
+        private int skillPanelHeight => 40;
+
+        public void InizializeSettings()
+        {
+            ReadPlayerSettings();
+            if (isFullScreen)
+            {
+                windowSizeWithPanel = GetMonitorResolution();
+            }
+            PreprocessSettings();
+        }
+
+        /// <summary>
+        /// Sets the default values for the various screen settings.
+        /// </summary>
+        private void SetDefault()
+        {
+            windowSizeWithPanel = new Size(320, 200);
+            windowSize = new Size(320, 160);
+            zoomFactor = 1;
+            isFullScreen = false;
+            isFillBorder = false;
+            isCompactSkillPanel = true;
+        }
+
+        /// <summary>
+        /// Reads the NeoLemmix player settings.
+        /// </summary>
+        private void ReadPlayerSettings()
+        {
+            try
+            {
+                int windowWidth = 320;
+                int windowHeight = 200;
+
+                string filePath = C.AppPath + "NeoLemmix147Settings.ini";
+                var fileReader = new StreamReader(filePath);
+
+                string line;
+                while ((line = fileReader.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    int seperatorIndex = line.IndexOf('=');
+                    int value = (seperatorIndex > 0) ? int.Parse(line.Substring(seperatorIndex + 1)) : 0;
+                    if (line.StartsWith("ZoomLevel"))
+                    {
+                        zoomFactor = value;
+                    }
+                    else if (line.StartsWith("FullScreen"))
+                    {
+                        isFullScreen = (value == 1);
+                    }
+                    else if (line.StartsWith("WindowWidth"))
+                    {
+                        windowWidth = value;
+                    }
+                    else if (line.StartsWith("WindowHeight"))
+                    {
+                        windowHeight = value;
+                    }
+                    else if (line.StartsWith("CompactSkillPanel"))
+                    {
+                        isCompactSkillPanel = (value == 1);
+                    }
+                    else if (line.StartsWith("IncreaseZoom"))
+                    {
+                        isFillBorder = (value == 1);
+                    }
+                }
+
+                windowSizeWithPanel = new Size(windowWidth, windowHeight);
+
+                fileReader.Dispose();
+            }
+            catch
+            {
+                SetDefault();
+            }
+        }
+
+        /// <summary>
+        /// Gets the screen resolution of the monitor.
+        /// </summary>
+        /// <returns></returns>
+        private Size GetMonitorResolution()
+        {
+            int monitorWidth = 1;
+            int monitorHeight = 1;
+            return new Size(monitorWidth, monitorHeight);
+        }
+
+        /// <summary>
+        /// Computes the window size used for the level
+        /// </summary>
+        private void PreprocessSettings()
+        {
+            int skillPanelZoom = Math.Min(windowSize.Width / skillPanelWidth, zoomFactor);
+            int skillPanelScreenHeight = skillPanelHeight * skillPanelZoom;
+            windowSize = new Size(windowSizeWithPanel.Width, windowSizeWithPanel.Height - skillPanelScreenHeight);
+        }
+
+        /// <summary>
+        /// Returns the size of the level area that is covered by the window screen.
+        /// </summary>
+        /// <param name="levelSize"></param>
+        /// <returns></returns>
+        public Size ScreenArea(int levelWidth, int levelHeight)
+        {
+            int zoom = zoomFactor;
+
+            if (isFillBorder)
+            {
+                // Round up on the separate zoom factors.
+                int zoomHoriz = (windowSize.Width + levelWidth - 1) / levelWidth;
+                int zoomVert = (windowSize.Height + levelHeight - 1) / levelHeight;
+                zoom = Math.Max(zoomHoriz, zoomVert);
+            }
+
+            // Round up on the screen width and height.
+            int screenWidth = (windowSize.Width + zoom - 1) / zoom;
+            int screenHeight = (windowSize.Height + zoom - 1) / zoom;
+
+            if (!isFillBorder)
+            {
+                screenWidth = Math.Max(screenWidth, levelWidth);
+                screenHeight = Math.Max(screenHeight, levelHeight);
+            }
+
+            return new Size(screenWidth, screenHeight);
+        }
+
+    }
+}
