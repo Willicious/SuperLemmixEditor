04d691f1d2ed3641cfeb51cf835dcf7e4f78921d
Stephan <st.neupert@web.de>
2017-06-14 19:53:12 +0200

    Updated version number to V0.7.

--- NLEditor/Properties/AssemblyInfo.cs
+++ NLEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NLEditor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An editor for NeoLemmix.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NLEditor")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
