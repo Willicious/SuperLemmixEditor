9851cc7b9d0ff17769ec6d19299d356b191e500d
Stephan <st.neupert@web.de>
2017-09-04 17:11:04 +0200

    Version number now 0.9.

--- NLEditor/Properties/AssemblyInfo.cs
+++ NLEditor/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NLEditor")]
 [assembly: AssemblyDescription("An editor for NeoLemmix.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Stephan Neupert")]
 [assembly: AssemblyProduct("NLEditor")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("CC BY-NC 4.0 (2017)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
