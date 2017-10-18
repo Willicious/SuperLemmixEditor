49e93d1296056c99f8627e81b0e7b4006e366b76
Stephan <st.neupert@web.de>
2017-10-18 18:35:27 +0200

    Replaced the NumericUpDowns with ones that select the whole number upon entering them to better overwrite them.

---
+++ NLEditor/NumUpDownOverwrite.cs
@@ -0,0 +1,45 @@
+﻿/* This class is heavily inspired by 
+ * https://stackoverflow.com/questions/571074/how-to-select-all-text-in-winforms-numericupdown-upon-tab-in */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NLEditor
+{
+    /// <summary>
+    /// A NumericUpDown that selects the whole number every time the user enters the control.
+    /// </summary>
+    class NumUpDownOverwrite : NumericUpDown
+    {
+        public NumUpDownOverwrite() : base()
+        {
+            this.Enter += NumUpDownOverwrite_Enter;
+            this.MouseDown += NumUpDownOverwrite_MouseDown;
+        }
+
+        bool selectByMouse = false;
+
+        private void NumUpDownOverwrite_Enter(object sender, EventArgs e)
+        {
+            Select();
+            Select(0, Text.Length);
+            if (MouseButtons == MouseButtons.Left)
+            {
+                selectByMouse = true;
+            }
+        }
+
+        private void NumUpDownOverwrite_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (selectByMouse)
+            {
+                Select(0, Text.Length);
+                selectByMouse = false;
+            }
+        }
+    }
+}
