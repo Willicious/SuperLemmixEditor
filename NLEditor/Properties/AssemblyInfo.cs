e2f3a6cf45e21c1a15de6ee806eee790b0050940
Namida Verasche <namida.verasche@gmail.com>
2020-07-02 13:47:21 +1200

    Version number update.
    (cherry picked from commit 3b93e23c3dc78f92b08def1a53e72818233a9831)

--- NLEditor/Properties/AssemblyInfo.cs
+++ NLEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Stephan Neupert & Namida Verasche")]
 [assembly: AssemblyProduct("NLEditor")]
-[assembly: AssemblyCopyright("CC BY-NC 4.0 (2017-2019)")]
+[assembly: AssemblyCopyright("CC BY-NC 4.0 (2017-2020)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.23.0.0")]
-[assembly: AssemblyFileVersion("1.23.0.0")]
+[assembly: AssemblyVersion("1.24.0.0")]
+[assembly: AssemblyFileVersion("1.24.0.0")]
